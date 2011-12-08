f36390ea9f120fd1704b9f36c0d13f9016360c29
Dan Schimpf <dschimpf@me.com>
2011-12-07 21:49:04 -0800

    add global hotkeys for start/stop and reset

--- Super Splits/Super Splits/SSAppDelegate.m
+++ Super Splits/Super Splits/SSAppDelegate.m
@@ -8,6 +8,25 @@
 
 #import "SSAppDelegate.h"
 
+#define START_STOP_HOT_KEY_ID 'stss'
+#define RESET_HOT_KEY_ID 'strt'
+
+static pascal OSStatus HotKeyHandler(EventHandlerCallRef nextHandler, EventRef theEvent, void * userData) {
+	EventHotKeyID hotKeyID;
+	GetEventParameter(theEvent, kEventParamDirectObject, typeEventHotKeyID, NULL, sizeof(hotKeyID) ,NULL, &hotKeyID);
+	
+	SSAppDelegate * appDelegate = (__bridge SSAppDelegate*)userData;
+	
+	if (hotKeyID.signature == START_STOP_HOT_KEY_ID) {
+		[appDelegate togglePause:nil];
+	}
+	else if (hotKeyID.signature == RESET_HOT_KEY_ID) {
+		[appDelegate resetRun:nil];
+	}
+	
+	return noErr;
+}
+
 @implementation SSAppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
@@ -15,6 +34,33 @@
     _windowController = [[SSWindowController alloc] initWithWindowNibName:@"MainWindow"];
     _mainController = [[SSMainController alloc] init];
     _windowController.mainController = _mainController;
+	
+	_hotKeyEventHandler = NewEventHandlerUPP(HotKeyHandler);
+	
+	EventTypeSpec eventType = {
+		.eventClass = kEventClassKeyboard,
+		.eventKind = kEventHotKeyPressed,
+	};
+	
+	InstallApplicationEventHandler(_hotKeyEventHandler, 1, &eventType, (__bridge void*)self, NULL);
+	
+	{
+		EventHotKeyID startStopKeyID = {
+			.signature = START_STOP_HOT_KEY_ID,
+		};
+		
+		// F15
+		RegisterEventHotKey(113, 0, startStopKeyID, GetApplicationEventTarget(), 0, &_startStopHotKeyRef);
+	}
+	
+	{
+		EventHotKeyID resetKeyID = {
+			.signature = RESET_HOT_KEY_ID,
+		};
+		
+		// F13
+		RegisterEventHotKey(105, 0, resetKeyID, GetApplicationEventTarget(), 0, &_resetHotKeyRef);
+	}
 
     // Force the controller to load (and show) the window:
     [_windowController window];
