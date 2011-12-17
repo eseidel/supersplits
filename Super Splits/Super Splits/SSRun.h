d4136151be82d124951c2a205e5ee59c9ddcef41
Eric Seidel <github@eseidel.com>
2011-12-16 23:00:00 -0800

    Attempt to add a "Close Window" menu item.  Doesn't work yet.

---
+++ Super Splits/Super Splits/SSRun.h
@@ -0,0 +1,13 @@
+//
+//  SSRun.h
+//  Super Splits
+//
+//  Created by Eric Seidel on 12/16/11.
+//  Copyright (c) 2011 Google. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SSRun : NSObject
+
+@end
---
+++ Super Splits/Super Splits/SSRun.m
@@ -0,0 +1,13 @@
+//
+//  SSRun.m
+//  Super Splits
+//
+//  Created by Eric Seidel on 12/16/11.
+//  Copyright (c) 2011 Google. All rights reserved.
+//
+
+#import "SSRun.h"
+
+@implementation SSRun
+
+@end
