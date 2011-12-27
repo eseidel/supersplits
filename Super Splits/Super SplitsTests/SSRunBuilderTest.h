eb6e74ceb645d0e38a0cd3784937a5989cc912ea
Eric Seidel <github@eseidel.com>
2011-12-26 23:54:27 -0600

    Add some super-basic unit testing.

---
+++ Super Splits/Super SplitsTests/SSRunBuilderTest.h
@@ -0,0 +1,13 @@
+//
+//  SSRunBuilderTest.h
+//  Super Splits
+//
+//  Created by Eric Seidel on 12/26/11.
+//  Copyright (c) 2011 Eric Seidel. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SSRunBuilderTest : SenTestCase
+
+@end
---
+++ Super Splits/Super SplitsTests/SSRunBuilderTest.m
@@ -0,0 +1,20 @@
+//
+//  SSRunBuilderTest.m
+//  Super Splits
+//
+//  Created by Eric Seidel on 12/26/11.
+//  Copyright (c) 2011 Eric Seidel. All rights reserved.
+//
+
+#import "SSRunBuilderTest.h"
+#import "SSRunBuilder.h"
+
+@implementation SSRunBuilderTest
+
+- (void)testInit
+{
+    SSRunBuilder *runBuilder = [[SSRunBuilder alloc] init];
+    STAssertEquals(runBuilder.state, UnknownState, @"SSRunBuilder starts at unknown");
+}
+
+@end
