e198428e0a99b3fd452999f0e3aa32710839b73e
Eric Seidel <github@eseidel.com>
2012-02-12 16:32:40 -0800

    Test the incremental matcher comparing two different runs.

--- Super Splits/Super SplitsTests/SSSplitMatcherTest.m
+++ Super Splits/Super SplitsTests/SSSplitMatcherTest.m
@@ -44,14 +44,12 @@
     }
 }
 
-- (void)testIncrementalSelfMatch
+- (NSArray *)_matchedSplitsFromIncrementalMatchOfRun:(SSRun *)run withRefernce:(SSRun *)referenceRun
 {
-    SSRun *run = [self _loadRun:@"6Jan2012"];
-    
     SSRunComparison *comparison = [SSRunComparison new];
     SSRunBuilder *runBuilder = [SSRunBuilder new];
     comparison.runBuilder = runBuilder;
-    comparison.referenceRun = run;
+    comparison.referenceRun = referenceRun;
     
     // Pretend we got the map information .5s after entering the room (common).
     const float kEntryMapStateOffset = 0.5;
@@ -60,12 +58,12 @@
     NSUInteger splitCount = run.roomSplits.count;
     for (NSUInteger splitIndex = 0; splitIndex < splitCount; splitIndex++) {
         SSSplit *split = [run.roomSplits objectAtIndex:splitIndex];
-
+        
         // Pretend we spent 1.5 seconds in the previous door/cutscene, but now enter a room.
         runBuilder.offset += 1.5;
         runBuilder.state = RoomState;
         [comparison _updateMatchedSplits];
-
+        
         // Update the map state after an offset.
         runBuilder.offset += kEntryMapStateOffset; 
         runBuilder.state = RoomState;
@@ -77,8 +75,15 @@
         runBuilder.state = RoomTransitionState;
         [comparison _updateMatchedSplits];
     }
+    return comparison.matchedSplits;
+}
 
-    NSArray *matchedSplits = comparison.matchedSplits;
+- (void)testIncrementalSelfMatch
+{
+    SSRun *run = [self _loadRun:@"6Jan2012"];
+    NSArray *matchedSplits = [self _matchedSplitsFromIncrementalMatchOfRun:run withRefernce:run];
+
+    NSUInteger splitCount = run.roomSplits.count;
     STAssertEquals(matchedSplits.count, splitCount, @"matcher split count");
     for (NSUInteger splitIndex = 0; splitIndex < splitCount; splitIndex++) {
         SSMatchedSplit *matchedSplit = [matchedSplits objectAtIndex:splitIndex];
@@ -87,6 +92,25 @@
 
         SSSplit *split = [run.roomSplits objectAtIndex:splitIndex];
         STAssertEquals(matchedSplit.referenceSplit, split, @"referenceSplit should match");
+        STAssertEqualObjects(matchedSplit.split.entryMapState, split.entryMapState, @"entryMapState should match");
+        STAssertEquals((int)matchedSplit.split.duration, (int)split.duration, @"duration should match");    
+    }
+}
+
+- (void)testIncrementalMatch
+{
+    SSRun *run = [self _loadRun:@"6Jan2012"];
+    SSRun *referenceRun = [self _loadRun:@"6Jan2012-reference"];
+    NSArray *matchedSplits = [self _matchedSplitsFromIncrementalMatchOfRun:run withRefernce:referenceRun];
+
+    NSUInteger splitCount = run.roomSplits.count;
+    STAssertEquals(matchedSplits.count, splitCount, @"matcher split count");
+    for (NSUInteger splitIndex = 0; splitIndex < splitCount; splitIndex++) {
+        SSMatchedSplit *matchedSplit = [matchedSplits objectAtIndex:splitIndex];
+        STAssertEquals(matchedSplit.splitIndex, splitIndex, @"splitIndex should match");
+        STAssertEquals(matchedSplit.referenceSplitIndex, splitIndex, @"referenceSplitIndex should match");
+        
+        SSSplit *split = [run.roomSplits objectAtIndex:splitIndex];
         STAssertEqualObjects(matchedSplit.split.entryMapState, split.entryMapState, @"entryMapState should match");
         STAssertEquals((int)matchedSplit.split.duration, (int)split.duration, @"duration should match");    
     }
